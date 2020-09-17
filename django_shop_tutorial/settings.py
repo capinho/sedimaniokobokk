1dfb25940e92a3c3710d734c5085cc5a70ec0515
Pape ibrahima diawara <papadiawara@hotmail.fr>
2020-09-17 03:32:26 +0000

    correction de bug

--- django_shop_tutorial/settings.py
+++ django_shop_tutorial/settings.py
@@ -63,6 +63,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'django_shop_tutorial.urls'
